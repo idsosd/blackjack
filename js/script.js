67616dac636092382adaf0064108919e60349a2f
ip.osinga <ip.osinga@alfa-college.nl>
2023-09-29 12:24:09 +0200

    v2023-09-29

--- js/script.js
+++ js/script.js
@@ -52,22 +52,71 @@ const cards = ["2_of_clubs.png",
     "10_queen_of_spades.png"
 ]
 let cardamount = 2
+let cardamountdealer = 1
 let random_nmbr = 0
 let playerpoints = 0
+let dealerpoints = 0
+
 random_nmbr = Math.floor(Math.random()*52)
 document.getElementById("playercard_1").src = "img/" + cards[random_nmbr]
 let card1value = cards[random_nmbr].split("_")
+
 playerpoints = parseInt(card1value[0])
+
 random_nmbr = Math.floor(Math.random()*52)
 document.getElementById("playercard_2").src = "img/" + cards[random_nmbr]
 let card2value = cards[random_nmbr].split("_")
-playerpoints += parseInt(card2value[0])
-document.getElementById("points").innerText = playerpoints
 
-function giveCard()
+playerpoints += parseInt(card2value[0])
+document.getElementById("playerpoints").innerText = playerpoints
+
+random_nmbr = Math.floor(Math.random()*52)
+document.getElementById("dealercard_1").src = "img/" + cards[random_nmbr]
+let dealercard1value = cards[random_nmbr].split("_")
+
+dealerpoints += parseInt(dealercard1value[0])
+document.getElementById("dealerpoints").innerText = dealerpoints
+
+function give()
 {
     let cardpushnumber = cardamount + 1
     random_nmbr = Math.floor(Math.random()*52)
     document.getElementById("playercard_" + cardpushnumber).src = "img/" + cards[random_nmbr]
+    
+    let nextcardvalue = cards[random_nmbr].split("_")
+    playerpoints += parseInt(nextcardvalue[0])
+    document.getElementById("playerpoints").innerText = playerpoints
+
     cardamount++
+}
+
+function givedealer()
+{
+    let cardpushnumber = cardamountdealer + 1
+    random_nmbr = Math.floor(Math.random()*52)
+    document.getElementById("dealercard_" + cardpushnumber).src = "img/" + cards[random_nmbr]
+    
+    let nextcardvalue = cards[random_nmbr].split("_")
+    dealerpoints += parseInt(nextcardvalue[0])
+    document.getElementById("dealerpoints").innerText = dealerpoints
+
+    cardamountdealer++
+}
+
+function stand()
+{
+    //de geef knop de-activeren
+    document.getElementById("givebtn").disabled = true
+    //de kaarten voor de dealer moeten gegeven worden volgens de regels die de dealer heeft
+    //sws stoppen bij 17 of hoger
+    //als 22 of hoger --> player wint
+    //als 17 t/m 21: vergelijken met wat de player heeft en dan bepalen wie er gewonnen heeft
+    //vraag: stel de player heeft 14, mag de dealer dan stoppen bij 15?
+    while(dealerpoints < 17)
+    {
+        //geef een kaart aan de dealer en werk de punten bij
+        givedealer();
+        break
+    }
+    //bepaal wie er gewonnen heeft
 }
