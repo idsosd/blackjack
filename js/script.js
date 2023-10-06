079d3708cc653418954661d8cb9af2e1b3f56c09
ip.osinga <ip.osinga@alfa-college.nl>
2023-10-06 13:02:51 +0200

    v2023-10-06

--- js/script.js
+++ js/script.js
@@ -51,72 +51,117 @@ const cards = ["2_of_clubs.png",
     "10_queen_of_hearts.png",
     "10_queen_of_spades.png"
 ]
-let cardamount = 2
+let cardamount = 0
 let cardamountdealer = 1
 let random_nmbr = 0
 let playerpoints = 0
+let orplayerpoints = 0
 let dealerpoints = 0
+let playercardpoints = [];
+let orplayercardpoints = [];
+startgame()
 
-random_nmbr = Math.floor(Math.random()*52)
-document.getElementById("playercard_1").src = "img/" + cards[random_nmbr]
-let card1value = cards[random_nmbr].split("_")
+function startgame()
+{
+    give()
+    give()
+    //als black jack meteen end-game met blackjack
+    if(playerpoints == 21)
+        endgame(1)
 
-playerpoints = parseInt(card1value[0])
+    random_nmbr = Math.floor(Math.random()*52)
+    document.getElementById("dealercard_1").src = "img/" + cards[random_nmbr]
+    let dealercard1value = cards[random_nmbr].split("_")
 
-random_nmbr = Math.floor(Math.random()*52)
-document.getElementById("playercard_2").src = "img/" + cards[random_nmbr]
-let card2value = cards[random_nmbr].split("_")
-
-playerpoints += parseInt(card2value[0])
-document.getElementById("playerpoints").innerText = playerpoints
-
-random_nmbr = Math.floor(Math.random()*52)
-document.getElementById("dealercard_1").src = "img/" + cards[random_nmbr]
-let dealercard1value = cards[random_nmbr].split("_")
-
-dealerpoints += parseInt(dealercard1value[0])
-document.getElementById("dealerpoints").innerText = dealerpoints
+    dealerpoints += parseInt(dealercard1value[0])
+    document.getElementById("dealerpoints").innerText = dealerpoints
+}
 
 function give()
 {
     let cardpushnumber = cardamount + 1
     random_nmbr = Math.floor(Math.random()*52)
     document.getElementById("playercard_" + cardpushnumber).src = "img/" + cards[random_nmbr]
-    
-    let nextcardvalue = cards[random_nmbr].split("_")
-    playerpoints += parseInt(nextcardvalue[0])
+    let cardvalue = cards[random_nmbr].split("_")
+    //plaats deze waarde in de array playercardspoints
+    playercardpoints.push(cardvalue[0])
+    if(parseInt(cardvalue[0]) == 11){
+        orplayercardpoints.push('1')
+    }
+    else {
+        orplayercardpoints.push(cardvalue[0])
+    }
+
+    playerpoints = countplayerpoints()
+    orplayerpoints = countorplayerpoints()
     document.getElementById("playerpoints").innerText = playerpoints
+    if(playerpoints != orplayerpoints){
+        document.getElementById("or_playerpoints").innerText = orplayerpoints
+    }
 
     cardamount++
+    if(playerpoints > 21 && orplayerpoints > 21){
+        endgame(0)
+    }
 }
 
 function givedealer()
 {
+    if(dealerpoints >= 17)
+        return
+    setTimeout(function(){ givedealer() }, 2000);
     let cardpushnumber = cardamountdealer + 1
     random_nmbr = Math.floor(Math.random()*52)
     document.getElementById("dealercard_" + cardpushnumber).src = "img/" + cards[random_nmbr]
-    
+    cardamountdealer++;
     let nextcardvalue = cards[random_nmbr].split("_")
     dealerpoints += parseInt(nextcardvalue[0])
-    document.getElementById("dealerpoints").innerText = dealerpoints
-
-    cardamountdealer++
+    document.getElementById("dealerpoints").innerText = dealerpoints 
 }
 
 function stand()
 {
-    //de geef knop de-activeren
     document.getElementById("givebtn").disabled = true
-    //de kaarten voor de dealer moeten gegeven worden volgens de regels die de dealer heeft
-    //sws stoppen bij 17 of hoger
-    //als 22 of hoger --> player wint
-    //als 17 t/m 21: vergelijken met wat de player heeft en dan bepalen wie er gewonnen heeft
-    //vraag: stel de player heeft 14, mag de dealer dan stoppen bij 15?
-    while(dealerpoints < 17)
-    {
-        //geef een kaart aan de dealer en werk de punten bij
-        givedealer();
-        break
+    givedealer()
+}
+
+function endgame(doodofblackjack)
+{
+    if(doodofblackjack == 0){
+        document.getElementById("givebtn").disabled = true
+        document.getElementById("standbtn").disabled = true
+        document.getElementById("dood").classList.add("text-bg-danger")
+        document.getElementById("dood").innerText = "DOOD"
     }
-    //bepaal wie er gewonnen heeft
+    else if(doodofblackjack == 1){
+        document.getElementById("givebtn").disabled = true
+        document.getElementById("standbtn").disabled = true
+        document.getElementById("dood").classList.add("text-bg-success")
+        document.getElementById("dood").innerText = "BLACK JACK"
+    }
+}
+
+function proces_ace()
+{
+
+}
+
+function countplayerpoints()
+{
+    let aantalpoints = 0
+    for(teller = 0; teller < playercardpoints.length; teller++)
+    {
+        aantalpoints += parseInt(playercardpoints[teller])
+    }
+    return aantalpoints
+}
+
+function countorplayerpoints()
+{
+    let aantalpoints = 0
+    for(teller = 0; teller < orplayercardpoints.length; teller++)
+    {
+        aantalpoints += parseInt(orplayercardpoints[teller])
+    }
+    return aantalpoints
 }
